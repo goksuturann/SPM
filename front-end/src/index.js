56ee091effe0f41d99110d38f664c4ec80193230
ezgi <ezgiyvz401@gmail.com>
2020-12-12 18:44:44 +0300

    sample login page with backend connection/backend not implemented yet

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LogInPage from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LogInPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
