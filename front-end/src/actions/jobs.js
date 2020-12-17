2e430e142b47523d1a5e565a5d2451d10b7a23a1
ezgi <ezqi401@hotmail.com>
2020-12-17 16:47:01 +0300

    jobpost changes

---
+++ front-end/src/actions/jobs.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { stopSubmit } from 'redux-form';
+
+import {
+    USER_LOADING,
+    USER_LOADED,
+    AUTH_ERROR,
+    REGISTER_FAIL,
+    REGISTER_SUCCESS,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    JOB_POST_FAIL,
+    JOB_POST_SUCCESS,
+  } from './types';
+
+const backend = "http://127.0.0.1:8000"
+
+// POST JOB
+export const post_job = ({job_title, keywords, salary,min_req,recommend_req},oed, employer_id) => async dispatch => {
+    // Headers
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+      // Request Body
+      
+      const body = JSON.stringify({job_title, keywords, salary,min_req,recommend_req,"employer_id":employer_id.id,"offer_end_date":oed });
+  
+      try {
+        const res = await axios.post(backend+'/api/jobs/create-job/', body, config);
+        dispatch({
+          type: JOB_POST_SUCCESS,
+          payload: res.data
+        });
+      } catch (err) {
+        dispatch({
+          type: JOB_POST_FAIL
+        });
+      }
+  };
+  
