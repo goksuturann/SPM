c1be5bf3bcd6ee81d49e717c708a831e50144e46
goncayilmaz <goncayilmaz2@outlook.com>
2020-12-17 20:21:10 +0300

    job post changes

--- backend/backend/jobs/urls.py
+++ backend/backend/jobs/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from jobs import views
+from .views import JobViewSet
 
-router = routers.DefaultRouter()
-router.register(r'jobs', views.JobViewSet, basename="jobs")
+router = routers.SimpleRouter()
+router.register(r'jobs', JobViewSet, basename="jobs")
 
 urlpatterns = [
     path('', include(router.urls)),
