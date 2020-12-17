e8eb7f3ec7a400794989eb0577d1126c800800e9
ezgi <ezqi401@hotmail.com>
2020-12-17 17:15:41 +0300

    modification to add single applicant to job front

--- front-end/src/actions/jobs.js
+++ front-end/src/actions/jobs.js
@@ -12,6 +12,8 @@ import {
     LOGOUT_SUCCESS,
     JOB_POST_FAIL,
     JOB_POST_SUCCESS,
+    APPLICATION_SUCCESS,
+    APPLICATION_FAIL
   } from './types';
 
 const backend = "http://127.0.0.1:8000"
@@ -30,6 +32,30 @@ export const post_job = ({job_title, keywords, salary,min_req,recommend_req},oed
   
       try {
         const res = await axios.post(backend+'/api/jobs/create-job/', body, config);
+        dispatch({
+          type: JOB_POST_SUCCESS,
+          payload: res.data
+        });
+      } catch (err) {
+        dispatch({
+          type: JOB_POST_FAIL
+        });
+      }
+  };
+
+  export const add_applicant = (employee_id, job_id) => async dispatch => {
+    // Headers
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+      // Request Body
+      
+      const body = JSON.stringify({"employee_id":employee_id});
+  
+      try {
+        const res = await axios.post(backend+'/api/jobs/'+job_id+'/add-applicant/', body, config);
         dispatch({
           type: JOB_POST_SUCCESS,
           payload: res.data
